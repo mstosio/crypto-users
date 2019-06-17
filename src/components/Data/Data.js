6f0f86a0160347ce7d20022126d3393214f4cdd0
mstosio <michaustosio@gmail.com>
2019-06-17 22:08:45 +0200

    created basic validation

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -2,22 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Data = ({ onSubmit, onChange }) => (
-  <>
-    <form action="" onSubmit={onSubmit}>
-      <label htmlFor="nickname">Nickname</label>
-      <input type="text" id="nickname" required />
+  <form action="" onSubmit={onSubmit}>
+    <label htmlFor="nickname">Nickname</label>
+    <input type="text" id="nickname" name="nickname" onChange={onChange} />
 
-      <label htmlFor="email">Email</label>
-      <input type="text" id="nickname" required />
+    <label htmlFor="email">Email</label>
+    <input type="email" id="email" name="email" onChange={onChange} />
 
-      <label htmlFor="nickname">IP adress</label>
-      <input type="text" id="nickname" required />
+    <label htmlFor="ipadress">IP adress</label>
+    <input type="text" id="ipadress" name="ipadress" onChange={onChange} />
 
-      <button type="submit" onChange={onChange}>
-        Add User
-      </button>
-    </form>
-  </>
+    <button type="submit">Add User</button>
+  </form>
 );
 
 export default Data;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-cond-assign */
 import React from 'react';
 import Data from './Data/Data';
 import Users from './Users/Users';
@@ -5,20 +7,50 @@ import Users from './Users/Users';
 class Main extends React.Component {
   state = {};
 
+  validateForm = () => {
+    let { nickNameError, emailError, ipError } = '';
+    const { nickname, email, ipadress } = this.state;
+
+    if (!nickname) {
+      nickNameError = 'please provide Nickname';
+    }
+
+    if (!email) {
+      emailError = 'please provideEmail';
+    }
+
+    if (!ipadress) {
+      ipError = 'please provide Ip';
+    }
+
+    if (nickNameError || emailError || ipError) {
+      this.setState({ nickNameError, emailError, ipError });
+      return false;
+    }
+
+    return true;
+  };
+
   onChange = event => {
     event.preventDefault();
-    console.log('hi');
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   };
 
   onSubmit = event => {
     event.preventDefault();
-    console.log('click');
+    const validation = this.validateForm();
+
+    if (validation) {
+      alert('huuray');
+    }
   };
 
   render() {
     return (
       <>
-        <Data onChange={this.onChange} />
+        <Data onChange={this.onChange} onSubmit={this.onSubmit} />
         <Users />
       </>
     );
